f7dfb0c19ad2770ae7b0a730a7104beec2d540fd
Eden Nagar <enagarw@gmail.com>
2026-03-03 11:52:22 -0500

    Adding input for Scrum method with get connection methods

--- core/src/main/java/com/cps/bugtracker/ScrumMethodClass.java
+++ core/src/main/java/com/cps/bugtracker/ScrumMethodClass.java
@@ -1,4 +1,36 @@
 package com.cps.bugtracker;
 
+import java.util.Scanner;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.*;
+
+
 public class ScrumMethodClass extends BugTracker {
+
+
+    public Long cretatePBI(String name, String des){
+        try (Connection conn = DriverManager.getConnection("jdbc:duckdb:BugTracker.db")) {
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO product_backlog_items(name, description)"+
+                    "VALUES (?, ?) RETURNING id";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            pstmt.setString(2, des);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getLong("id");
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Database error: " + e.getMessage());
+        }
+        return null;
+
+    }
+
+
 }
